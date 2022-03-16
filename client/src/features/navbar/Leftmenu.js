77a64c2cabcf407feb5bd1a2c8f753350d50ce88
kimondo88 <kimondo1988@o2.pl>
2022-03-16 14:04:42 +0100

    add FA Icons dep, add LMenu with FA, also toggleLMenu display

--- client/src/features/navbar/Leftmenu.js
+++ client/src/features/navbar/Leftmenu.js
@@ -2,11 +2,13 @@ import styles from './Navbar.module.css'
 import { selectVisibility } from './navbarSlice';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faCompass, faBolt} from '@fortawesome/free-solid-svg-icons';
 export function LeftMenu(){
     return (
         <div>
             <LeftMenuShow/> 
+            <LeftMenuMin/>
         </div>
     )
 }
@@ -17,31 +19,46 @@ function LeftMenuShow(){
     useEffect(()=>{
         if(visible === false){
             setCounter(1);
-            return toggle();
+            return toggleHidden();
         }else if(counter !== 0){
-            return toggle();
+            return toggleHidden();
         }  
     // eslint-disable-next-line
     }, [visible]);
-    function toggle(){
-      let element = document.getElementById('menu');
-      element.classList.toggle(styles.leftBarTrans);
+    function toggleHidden(){
+        let element = document.getElementById('menu');
+        element.classList.toggle(styles.none);
     }
     return (
         <div id='menu' className={styles.leftBar + ' ' + styles.duration}>
-            <div>T</div>
-            <div>B</div>
-            <div>C</div>
+            <div className={styles.font}><FontAwesomeIcon className={styles.gridItem} icon={faHouse}/>Home</div>
+            <div className={styles.font}><FontAwesomeIcon className={styles.gridItem} icon={faCompass} />Discover</div>
+            <div className={styles.font}><FontAwesomeIcon className={styles.gridItem} icon={faBolt} />Shorts</div>
         </div>
     )
 }
 
-// function LeftMenuMin(){
-//     return (
-//         <div className={`${styles.leftBar} ${styles.duration} ${styles.leftBarTrans}`}>
-//             <div>T</div>
-//             <div>B</div>
-//             <div>C</div>
-//         </div>
-//     )
-// }
+function LeftMenuMin(){
+    const [counter, setCounter] = useState(0);
+    const visible = useSelector(selectVisibility);
+    useEffect(()=>{
+        if(visible === false){
+            setCounter(1);
+            return toggleHidden();
+        }else if(counter !== 0){
+            return toggleHidden();
+        }
+        // eslint-disable-next-line
+    }, [visible])
+    function toggleHidden(){
+        let element = document.getElementById('min');
+        element.classList.toggle(styles.none);
+    }
+    return (
+        <div id="min" className={`${styles.leftBarMin} ${styles.duration} ${styles.none}`}>
+            <div className={styles.fontMin + ' ' + styles.marginTop}><FontAwesomeIcon className={styles.gridItem} icon={faHouse}/></div>
+            <div className={styles.fontMin}><FontAwesomeIcon className={styles.gridItem} icon={faCompass} /></div>
+            <div className={styles.fontMin}><FontAwesomeIcon className={styles.gridItem} icon={faBolt} /></div>
+        </div>
+    )
+}
