dd94676ce91f579078741280c21627ca36bb8c9d
kimondo88 <kimondo1988@o2.pl>
2022-03-25 23:41:04 +0100

    add redux dev tools-extension for better inspect

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,16 +1,16 @@
 import { createStore } from '@reduxjs/toolkit';
 import { createBrowserHistory } from 'history'
 import { routerMiddleware } from 'connected-react-router';
-import { applyMiddleware, compose } from '@reduxjs/toolkit';
+import { applyMiddleware} from '@reduxjs/toolkit';
 import createRootReducer from './reducers';
-
+import { composeWithDevTools } from '@redux-devtools/extension';
 export const history = createBrowserHistory();
 
 export default function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history),
     preloadedState,
-    compose(
+    composeWithDevTools(
       applyMiddleware(
         routerMiddleware(history)
       )
